0e5b22c2143936dcdca11cc82bf1d3293f967d54
Nabil Yermani <nabilyermani@yahoo.de>
2024-04-08 16:02:53 +0200

    Homework added

---
+++ alter.js
@@ -0,0 +1,6 @@
+function summeAusgeben(zahl1, zahl2) {
+    const summe= zahl1 + zahl2;
+    console.log("Die Summe von " + zahl1 + " und " + zahl2 + " ist " + summe + " . "); 
+}
+
+module.exports = summeAusgeben;
---
+++ index.js
@@ -0,0 +1,5 @@
+const summeAusgeben = require('./alter')
+
+summeAusgeben(15, 15);
+summeAusgeben(24, 78);
+summeAusgeben(101, 567);
